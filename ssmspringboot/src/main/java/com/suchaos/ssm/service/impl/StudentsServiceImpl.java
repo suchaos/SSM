4748cf20d43fc70e9e92bd1464dcd99af699f835
suchaos <822075222@qq.com>
2018-12-27 11:06:17 +0800

    springboot ssm 整合

---
+++ ssmspringboot/src/main/java/com/suchaos/ssm/service/impl/StudentsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.suchaos.ssm.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.suchaos.ssm.dto.Pageable;
+import com.suchaos.ssm.dto.ResultType;
+import com.suchaos.ssm.mapper.StudentsMapper;
+import com.suchaos.ssm.model.Students;
+import com.suchaos.ssm.model.StudentsExample;
+import com.suchaos.ssm.service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * StudentsServiceImpl
+ *
+ * @author suchao
+ * @date 2018/12/26
+ */
+@Service
+public class StudentsServiceImpl implements StudentsService {
+    @Autowired
+    private StudentsMapper studentsMapper;
+
+    @Override
+    public Students getStudentBtId(Long id) {
+        return studentsMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public String getStudentList(int page, int size) {
+        PageHelper.startPage(page, size);
+        StudentsExample example = new StudentsExample();
+        StudentsExample.Criteria criteria = example.createCriteria();
+        List<Students> list = studentsMapper.selectByExample(example);
+        PageInfo pageInfo = new PageInfo<Students>(list);
+        return ResultType.resultList("success", list, new Pageable(pageInfo));
+    }
+}
